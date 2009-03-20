8448443614ffc3a52d0cbec1288cb9ca402528fb
Matthias Georgi <matti.georgi@gmail.com>
2009-03-20 17:06:29 +0100

    content-length was missing for http errors

--- lib/rack_dav/handler.rb
+++ lib/rack_dav/handler.rb
@@ -18,8 +18,9 @@ module RackDAV
         controller.send(request.request_method.downcase)
         
       rescue HTTPStatus::Status => status
-        response.status = status.code      
+        response.status = status.code
         response.body = status.message if status.code >= 300
+        response['Content-Length'] = response.body.size
       end
 
       response.status = response.status ? response.status.to_i : 200
