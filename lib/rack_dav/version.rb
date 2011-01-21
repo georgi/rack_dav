49283c3d923b9e324bdb19d2ec33d665d735102f
Simone Carletti <weppos@weppos.net>
2011-01-21 11:26:02 +0100

    Development release.

--- lib/rack_dav/version.rb
+++ lib/rack_dav/version.rb
@@ -1,3 +1,16 @@
 module RackDAV
-  VERSION = "0.1.3"
+
+  # Holds information about library version.
+  module Version
+    MAJOR = 0
+    MINOR = 2
+    PATCH = 0
+    BUILD = "dev#{Time.now.to_i}"
+
+    STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join(".")
+  end
+
+  # The current library version.
+  VERSION = Version::STRING
+
 end
