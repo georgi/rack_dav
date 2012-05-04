9cfee57b1f506fa53724e58473247e59c824c961
Ramon Marco L. Navarro <ramonmaruko@gmail.com>
2012-05-04 14:24:02 +0800

    Add support for Content-MD5 (RFC 1864)
    Change-Id: I091440eb736c4549dd93de6c885598f7e4e539f7

--- lib/rack_dav/file_resource.rb
+++ lib/rack_dav/file_resource.rb
@@ -83,6 +83,8 @@ module RackDAV
     #
     # Save the content of the request.body.
     def put(request, response)
+      content_md5_pass?(request.env) or raise HTTPStatus::BadRequest.new('Content-MD5 mismatch')
+
       write(request.body)
     end
 
@@ -160,6 +162,18 @@ module RackDAV
 
       def stat
         @stat ||= File.stat(file_path)
+      end
+
+      def content_md5_pass?(env)
+        expected = env['HTTP_CONTENT_MD5'] or return true
+
+        body   = env['rack.input'].dup
+        digest = Digest::MD5.new.digest(body.read)
+        actual = [ digest ].pack('m').strip
+
+        body.rewind
+
+        expected == actual
       end
 
   end
